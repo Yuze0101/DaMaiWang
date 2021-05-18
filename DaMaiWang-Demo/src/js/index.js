5f9e92f55d9d2c03b4f2164be5ad43f08967e3d4
Nevermore <mengmiao.0101@icloud.com>
2021-05-18 12:24:30 +0800

    master

---
+++ DaMaiWang-Demo/src/js/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: GengYuan
+ * @Date: 2021-05-18 12:24:07
+ * @LastEditTime: 2021-05-18 12:24:08
+ * @LastEditors: GengYuan
+ * @Description:
+ * @FilePath: \第二周项目副本\DaMaiWang\DaMaiWang-Demo\src\js\index.js
+ * 可以输入预定的版权声明、个性签名、空行等
+ */
